47a0b32d84cf708eec67a3eeae9b98afe9911939
sum12 <sumitjami@gmail.com>
2022-12-03 12:18:12 +0100

    part 1

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        panic!();
+    }
+
+    pub fn set(&mut self, k: String, v: String) {
+        panic!();
+    }
+
+    pub fn get(&mut self, k: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, k: String) {
+        panic!();
+    }
+}
